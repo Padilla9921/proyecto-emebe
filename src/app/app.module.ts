1e504ef87db7abe99b7518ce2bb4dc23c1e17768
Padilla9921 <jp992111@gmail.com>
2023-03-13 12:57:08 -0600

    13
    03

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
 import { InicioComponent } from './content/inicio/inicio.component';
+import { AngularFireModule } from '@angular/fire/compat';
 import { FooterComponent } from './footer/footer.component';
 import { RegistroComponent } from './registro/registro.component';
 import { ContentComponent } from './content/content.component';
@@ -48,6 +50,17 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyB6yBIB8wVXpi2i6JZjYlTJReEBnqWuoAQ",
+      authDomain: "muebles-la-espuela.firebaseapp.com",
+      projectId: "muebles-la-espuela",
+      storageBucket: "muebles-la-espuela.appspot.com",
+      messagingSenderId: "960928744373",
+      appId: "1:960928744373:web:cb3425b44e27b915a5cffc",
+      measurementId: "G-BLTEVYDRPY"}
+})
+],
     RouterModule.forRoot(appRoutes,{enableTracing:true})
   ],
   providers: [
