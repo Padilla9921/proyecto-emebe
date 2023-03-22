35e7537f3acc53ed239baadfbd0ec910f300bd59
Padilla9921 <jp992111@gmail.com>
2023-03-22 01:55:34 -0600

    22
    03

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+// import { AngularFireAuth } from '@angular/fire/auth';
+
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
-export class RegistroComponent {
+export class RegistroComponent  {
 
+form={
+nombre:"",
+correo:"",
+contrasena:""
+
+}
+ccontrasena:any
+
+
+constructor(){}
+
+ngOnInit(): void{
+
+}
+ 
+//registrarme
+registrarme(){
+  if(this.form.nombre != '' && this.form.correo != '' && this.contrasena != '' && this.ccontrasena!=''){
+    if(this.form)
+  }
+}
 }
